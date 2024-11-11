e989c7d6f3028b1c2a1e3abe4962f5338ec80049
alanakil <aakil@math.uh.edu>
2024-11-11 21:30:30 +0000

    moved prepare_dataset to helpers

--- src/helpers.py
+++ src/helpers.py
@@ -6,6 +6,54 @@ import seaborn as sns
 
 
 # %%
+def prepare_dataset(dataframe, policy, class_names):
+    dataset_df = dataframe[
+        dataframe["Frontal/Lateral"] == "Frontal"
+    ]  # take frontal pics only
+    # Check which images had a support device. This is optional.
+    # dataset_df = dataset_df[dataset_df["Support Devices"] == 1].reset_index(drop=True)
+    df = dataset_df.sample(
+        frac=1.0, random_state=1
+    )  # If desired, downsample the dataset.
+    df.fillna(0, inplace=True)  # fill the with zeros
+    x_path, y_df = df["Path"].to_numpy(), df[class_names]
+    class_ones = ["Atelectasis", "Cardiomegaly"]
+    y = np.empty(y_df.shape, dtype=int)
+    for i, (index, row) in enumerate(y_df.iterrows()):
+        labels = []
+        for cls in class_names:
+            curr_val = row[cls]
+            feat_val = 0
+            if curr_val:
+                curr_val = float(curr_val)
+                if curr_val == 1:
+                    feat_val = 1
+                elif curr_val == -1:
+                    if policy == "ones":
+                        feat_val = 1
+                    elif policy == "zeroes":
+                        feat_val = 0
+                    elif policy == "mixed":
+                        if cls in class_ones:
+                            feat_val = 1
+                        else:
+                            feat_val = 0
+                    else:
+                        feat_val = 0
+                else:
+                    feat_val = 0
+            else:
+                feat_val = 0
+
+            labels.append(feat_val)
+
+        y[i] = labels
+
+    x_path = ["./" + x for x in x_path]
+
+    return x_path, y
+
+# %%
 def roc_curves(class_names, all_outputs, all_targets):
     auc_per_class = {}
     plt.figure(figsize=(10, 8))
